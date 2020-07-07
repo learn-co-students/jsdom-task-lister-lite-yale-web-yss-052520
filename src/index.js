f4e06ac87df7bc555d16d36ce730b070338e6cf3
dlee3427 <david.lee.sl2634@yale.edu>
2020-07-07 15:43:48 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  const newForm = document.getElementById("create-task-form")
+  const taskDescription = document.getElementById("new-task-description")
+  newForm.addEventListener("submit", createTask)
+  
+  const newExercise = document.getElementById("create-new-exercise")
+  const exerciseDescription = document.getElementById("new-exercise-description")
+  newExercise.addEventListener("submit", newExercise)
+
 });
+
+const createTask = event => {
+  event.preventDefault()
+  const taskDescription = document.getElementById("new-task-description")
+  const newTask = document.createElement("li")
+  newTask.innerHTML = taskDescription.value 
+  document.getElementById("tasks").appendChild(newTask);
+  alert("New Task!")
+}
+
+// still trying to figure out how to implement this event
+const deleteTask = event => {
+  event.preventDefault()
+  const taskDescription = document.getElementById("new-task-description")
+  taskDescription.remove()
+  alert("You have deleted a task.")
+}
+
+const newExercise = event => {
+  event.preventDefault()
+  const exerciseDescription = document.getElementById("new-exercise-description")
+  const newActivity = document.createElement("li")
+  newActivity.innerHTML = exerciseDescription.value
+  document.getElementById("exercise").appendChild(newActivity)
+}
