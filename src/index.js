3ca3b46b97082e9b39722a591e19443afbe8614e
nicolewang2000 <nicole.wang@yale.edu>
2020-07-07 16:12:18 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", function () {
+  const taskForm = document.getElementById("create-task-form") 
+  taskForm.addEventListener('submit', function(event) {
+    event.preventDefault() //stop form from POSTING
+    const userInputField = event.target.querySelector("#new-task-description")  //gets user input field
+    //OR document.querySelector("#new-task-description")
+    const todoList = document.querySelector("#tasks") //fetches the task box
+    const todoPTag = document.createElement('p') //creates a new p-tag
+    todoPTag.textContent = userInputField.value //saves the value of the input field as the p-tag content
+    todoList.appendChild(todoPTag) //appends p-tag to the todo list
+    document.forms['create-task-form'].reset() // resets the input field after pressing submit
+  })
 });
