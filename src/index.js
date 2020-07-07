aa76190c8851643bb74fb0cff621808d0ab9430e
Dennis <Dennis.portillo@yale.edu>
2020-07-07 12:35:30 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.getElementById('create-task-form')
+  form.addEventListener("submit", submitForm)
+})
+
+function getTasks() {
+  return document.getElementById('tasks')
+}
+
+function submitForm(event) {
+  event.preventDefault()
+
+  let newTaskDesc = document.getElementById('new-task-description');
+  
+  let taskList = document.createElement('li');
+  taskList.innerText = newTaskDesc.Value;
+  
+  getTasks().appendChild(taskList)
+  event.target.reset();
+}
