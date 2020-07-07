31147b90693d44cfea2a9e6077b3d6e4d9e27be7
ZIming Mao <ziming.mao@yale.edu>
2020-07-07 14:37:23 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  const todoForm = document.getElementById('create-task-form')
+  const taskContainer = document.getElementById('tasks')
+
+  todoForm.addEventListener('submit', function(event) {
+    event.preventDefault()
+    let task = event.target.querySelector("#new-task-description").value
+
+    const taskLiTag = document.createElement('li')
+    taskLiTag.textContent = task + " "
+    const buttonTag = document.createElement('button')
+    buttonTag.textContent = "X"
+    buttonTag.className = "delete_button"
+    buttonTag.addEventListener('click', function(){
+      buttonTag.parentElement.remove()
+    })
+    taskLiTag.appendChild(buttonTag)
+    taskContainer.appendChild(taskLiTag)
+
+  })
+
+
 });
