16db5981c53d5380ba906111696aab159a980000
eric <eric.ou@yale.edu>
2020-07-07 13:00:42 -0700

    finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let task_form = document.querySelector("#create-task-form");
+  let form_val = document.querySelector("#new-task-description");
+  let tasks = document.querySelector("#tasks")
+  task_form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    let item = document.createElement("li");
+    item.innerText = form_val.value;
+    tasks.append(item);
+    e.target.reset();
+  });
 });
