fc5b79edcc6103285be36e51b0156911269760cb
nicolewang2000 <nicole.wang@yale.edu>
2020-07-07 16:34:15 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
   const taskForm = document.getElementById("create-task-form") 
+  
   taskForm.addEventListener('submit', function(event) {
     event.preventDefault() //stop form from POSTING
     const userInputField = event.target.querySelector("#new-task-description")  //gets user input field
     //OR document.querySelector("#new-task-description")
+
     const todoList = document.querySelector("#tasks") //fetches the task box
-    const todoPTag = document.createElement('p') //creates a new p-tag
-    todoPTag.textContent = userInputField.value //saves the value of the input field as the p-tag content
-    todoList.appendChild(todoPTag) //appends p-tag to the todo list
-    document.forms['create-task-form'].reset() // resets the input field after pressing submit
+    const listElements = document.createElement('li') //creates a new list
+    listElements.textContent = userInputField.value //saves the value of the input field as the p-tag content
+    todoList.appendChild(listElements) //appends p-tag to the todo list
+
+    const deleteButton = document.createElement('button') //creates a delete button 
+    deleteButton.innerText = "x" 
+    deleteButton.className = "delete"
+    todoList.appendChild(deleteButton)
+
+    deleteButton.onclick = function(event) { //function for delete button
+      todoList.removeChild(listElements)
+      todoList.removeChild(deleteButton)
+    }
+
+    taskForm.reset() // resets the input field after pressing submit
+   
   })
 });
