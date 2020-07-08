25a2f8218622e245b94ae32faf807a1508ab1203
ev275 <enrique.valencia@yale.edu>
2020-07-07 20:13:31 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("form#create-task-form")
+  let ul = document.querySelector("#tasks")
+
+  form.addEventListener("submit", function() {
+      let li = document.createElement("li")
+      li.innerText = document.querySelector("#new-task-description").value
+      ul.append(li)
+
+      event.preventDefault()
+  })
 });
