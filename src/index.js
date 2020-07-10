c44c00154e8904341f8c5d94e0e481ec3a9b2905
adilgondal1 <adil.gondal@yale.edu>
2020-07-10 02:14:01 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("form")
+  const todo = document.querySelector("ul")
+  form.addEventListener("submit",function(){
+    const li = document.createElement("li")
+    const btn = document.createElement("button")
+    btn.innerText = "X"
+    btn.addEventListener("click",function(){
+      let del = event.target.parentNode 
+      del.parentNode.removeChild(del)
+    })
+    const description = document.querySelector("input#new-task-description").value
+    li.innerText = description
+    console.log(li)
+    li.append(btn)
+    form.reset()
+    todo.append(li)
+    event.preventDefault()
+  })
 });
