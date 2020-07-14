95812032591d8887936a93e16d4479e99c8eb199
Jonkavaliauskas <jonas.kavaliauskas@yale.edu>
2020-07-15 00:15:48 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  function qs(selector){
+    return document.querySelector(selector)
+  }
+
+  function ce(element){
+    return document.createElement(element)
+  }
+
+  let form = qs("form#create-task-form")
+
+  form.addEventListener("submit", function(){
+    event.preventDefault()
+    let li = ce("li")
+    let btn = ce("button")
+    btn.innerText = "x"
+    li.innerText = qs("input#new-task-description").value
+    
+    btn.addEventListener("click", function(){
+      // console.log("event", event)
+      // console.log("eventtarget", event.target.id)
+      li.remove()
+    })
+    li.append(btn)
+
+
+    let taskDiv = qs("ul#tasks")
+    taskDiv.append(li)
+
+
+
+    event.target.reset()
+  })
+
 });
