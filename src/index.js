4d091725dc3efaca94851f5ba4654c03a215b662
davidyu7 <david.yu@yale.edu>
2020-07-17 01:15:57 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector("form#create-task-form")
+  const taskList = document.querySelector("ul#tasks")
+  form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    newTask = document.createElement("li")
+    newTask.innerHTML = e.target[0].value
+    taskList.appendChild(newTask)
+  })
 });
